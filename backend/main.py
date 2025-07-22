fe2234bf8c7fe78bfdd4d2c37fcbc044d41b1797
D3ltaD4ve <david.rossig12@gmail.com>
2025-07-22 11:50:12 +0200

    basic skeleton backend (localhost)

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, database, schemas
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI()
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/posts/", response_model=schemas.PostOut)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    db_post = models.Post(title=post.title, content=post.content)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+@app.get("/posts/{post_id}", response_model=schemas.PostOut)
+def read_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
